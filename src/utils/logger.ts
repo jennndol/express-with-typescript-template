447a784fe78c6b2f1d141dde96df8e31a188f594
Luthfi <jkt.luthfi@gmail.com>
2020-06-25 13:27:26 +0700

    ADD: eslint config

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 
-import winston, { createLogger, format, transports, child } from 'winston';
-import 'winston-daily-rotate-file';
-import { v4 } from 'uuid';
+import { createLogger, format, transports } from "winston";
+import "winston-daily-rotate-file";
+import { v4 } from "uuid";
 const { combine, timestamp, prettyPrint } = format;
 
-const transport = new (winston.transports.DailyRotateFile)({
-    filename: './logs/%DATE%.log',
-    datePattern: 'YYYY-MM-DD-HH',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d'
-  });
+const transport = new (transports.DailyRotateFile)({
+	filename: "./logs/%DATE%.log",
+	datePattern: "YYYY-MM-DD-HH",
+	zippedArchive: true,
+	maxSize: "20m",
+	maxFiles: "14d"
+});
 
 const logger = createLogger({
-    format: combine(
-        timestamp(),
-        prettyPrint(),
-    ),
-    transports: [
-        transport
-    ],
-    exitOnError: false,
+	format: combine(
+		timestamp(),
+		prettyPrint(),
+	),
+	transports: [
+		transport
+	],
+	exitOnError: false,
 });
 
 const children = logger.child({requestId: v4()});
 
 export const stream = {
-    write: (message: string) => {
-        children.info(message);
-    }
-}
+	write: (message: string):void => {
+		children.info(message);
+	}
+};
 
 export default children;
